2dbfc824df1629671435c0cd7d8b77b9821ac1c0
Tryd0g0lik <work80@mail.ru>
2023-09-18 13:01:34 +0700

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+		"es6": true,
+				jeat:true
+    },
+    "extends": "standard-with-typescript",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+	},
+	"parser": ["babel-eslint", '@typescript-eslint/parser']
+}
