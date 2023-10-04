15701d517e02c02d1b18291b60c8731275aefe4a
Tryd0g0lik <work80@mail.ru>
2023-10-04 15:44:14 +0700

    not rigth

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,57 @@
-
-function nowTime() {
-	let now = new Date();
-	return now;
-}
-self.addEventListener("install", async (ev: any) => {
-	// const { corrective } = require('../../frontend/src/ts/functions/index.ts');
-	console.log('Установлен!')
-	// https://developer.mozilla.org/ru/docs/Web/API/Service_Worker_API/Using_Service_Workers#установка_и_активация_заполнение_кеша
-
-	ev.waitUntil(
-		caches.open("my-first-cech")
+self.addEventListener('install', (ev: any) => {
+	function fun() {
+		caches.open('cache-page')
 			.then((cache: any) => {
 				return cache.addAll([
-					'./main.css',
-					'./pic/bg_buggy.png'
-				])
-			})
-	);
-
-});
-
-self.addEventListener('activate', (ev: any) => {
-	console.log('Активирован!')
-	function func() {
-		// const delay = (ms: any) => new Promise((resolve: any) => setTimeout(resolve, ms));
-		// await delay(0);
-
-		return caches.open("my-second-cech")
-			.then((cache: any) => {
-				cache.addAll([
 					'./',
 					'./index.html'
 				])
+			});
+		caches.open('cache-static')
+			.then((cache: any) => {
+				return cache.addAll([
+					'./main.css',
+					'./frontend.js'
+				])
 			})
 	}
-	ev.waitUntil(func())
-
-});
-
+	ev.waitUntil(
+		fun()
+	)
+})
+self.addEventListener('activate', (ev: any) => {
+	console.log('Activated');
+})
 self.addEventListener('fetch', (ev: any) => {
-	console.log('Запрос на сервер!')
-	// файлы для частого обновления
-
-
-	// self.addEventListener('message')
-	console.log('EV : ', ev.request);
-	ev
-		.respondWith(caches.match(ev.request)
+	ev.respondWith(
+		caches.match(ev.request)
 			.then((response: any) => {
-				// смотрим в кеш Если там ни чего нет делаем запрос  на сервер
-				console.log('RESPONSEЖ ', response)
-				// response != undefined ? response :
-				const result = undefined ? response : fetch('http://localhost:8080/')
-
-				return result
-
-			})
-			.then(async (response: any) => {
-				const timer = (ms: any) => new Promise((resolve: any) => setTimeout(resolve, ms));
-				await timer(20000);
-
-				console.log('RESPONSE.Text: ', response.text());
-				return caches.open("my-second-cech-new")
-					.then((cahce: any) => {
-
-						cahce.put(ev.request, response.text());
-						return response;
+				return response || fetch(ev.request)
+					.then((resolve: any) => {
+						if (ev.request && resolve) resolve.put(ev.request, response.clone())
 					})
 			})
 	)
 })
-console.log('SELF SELF: ', self);
 self.addEventListener('message', (ev: any) => {
-	caches.open('my-second-cech-new')
-		.then((cache: any) => {
-			debugger;
-			cache.add('./', ev.data['page'].slice(0,))
-			return ev
-		})
+	if (ev.data && ev.data['page']) {
+		// ev.waitUntil(
+		caches.open('cache-page')
+			.then((cache: any) => {
 
+				const pages = ['./', './index.html'];
+				for (let i = 0; i < pages.length; i++) {
+
+					cache.delete(pages[i])
+						.then((result: any) => {
+							debugger;
+
+							cache.put(pages[i], new Response(`<!DOCTYPE html><html>` + ev.data['page'] + `</html>`))
+						})
+				}
+
+			})
+		// )
+
+	}
 })
