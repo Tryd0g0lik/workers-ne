953b1f7aaf52d79e82da64570cb7e09bacb8dc1a
Tryd0g0lik <work80@mail.ru>
2023-10-06 12:41:43 +0700

    [CAHER}: serwiceWorker and cachingData from function/index.ts was completed

--- src/index.ts
+++ src/index.ts
@@ -18,40 +18,22 @@ self.addEventListener('install', (ev: any) => {
 	ev.waitUntil(
 		fun()
 	)
-})
-self.addEventListener('activate', (ev: any) => {
-	console.log('Activated');
-})
+});
+
+self.addEventListener('activate', (ev: any) => { console.log('Activated') });
+
 self.addEventListener('fetch', (ev: any) => {
 	ev.respondWith(
 		caches.match(ev.request)
 			.then((response: any) => {
-				return response || fetch(ev.request)
-					.then((resolve: any) => {
-						if (ev.request && resolve) resolve.put(ev.request, response.clone())
-					})
+				if (!response) return
+				return response
 			})
 	)
-})
+});
+
 self.addEventListener('message', (ev: any) => {
-	if (ev.data && ev.data['page']) {
-		// ev.waitUntil(
-		caches.open('cache-page')
-			.then((cache: any) => {
 
-				const pages = ['./', './index.html'];
-				for (let i = 0; i < pages.length; i++) {
-
-					cache.delete(pages[i])
-						.then((result: any) => {
-							debugger;
-
-							cache.put(pages[i], new Response(`<!DOCTYPE html><html>` + ev.data['page'] + `</html>`))
-						})
-				}
-
-			})
-		// )
-
-	}
 })
+
+
