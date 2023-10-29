c0eeeb8d2eeb0f0efbcf2fbc5c910b08450d77ca
Tryd0g0lik <work80@mail.ru>
2023-10-30 05:41:20 +0700

    [src\app\serwiceWorker\src\index.ts]: has been correted. Strategies for a Cache were  partitioned

--- src/app/serwiceWorker/src/index.ts
+++ src/app/serwiceWorker/src/index.ts
@@ -1,3 +1,47 @@
+// src\app\serwiceWorker\src\index.ts
+// стратегии кеша
+
+const CACHE_PRIORITY_URLS = ['/frontend.js'];
+const FETCH_PRIORITY_URLS = ['/', '/index.html', '/main.css', '/pic/bg_buggy.png']
+
+async function cachePriorityThenFeth(ev: any) {
+	let response: any;
+	const cacheResponse = await caches.match(ev.request);
+
+	if (cacheResponse) {
+		return cacheResponse
+	}
+	try {
+		response = await fetch(ev.request);
+
+	} catch (err: any) {
+		return
+	}
+
+	const cache = await caches.open('v1');
+	cache.put(ev.request, response.clone());
+
+	return response
+}
+
+async function fetchPriorityThenCache(ev: any) {
+	let response: any;
+
+	try {
+		response = await fetch(ev.request);
+
+	} catch (err: any) {
+		const cacheResponse = await caches.match(ev.request);
+		if (cacheResponse) { return cacheResponse }
+	}
+
+
+	const cache = await caches.open('v2');
+	cache.put(ev.request, response.clone());
+	return response
+}
+
+
 self.addEventListener('install', (ev: any) => {
 	function fun() {
 		caches.open('v1') // Создаём кеш с именем "cache-page"
@@ -6,6 +50,7 @@ self.addEventListener('install', (ev: any) => {
 					'./',
 					'./index.html',
 					'./main.css',
+					'./pic/bg_buggy.png'
 				])
 			});
 		caches.open('v2') // Создаём кеш который меняется редко с именем "cache-page"
@@ -22,14 +67,18 @@ self.addEventListener('install', (ev: any) => {
 self.addEventListener('activate', (ev: any) => { console.log('Activated') });
 
 self.addEventListener('fetch', (ev: any) => {
-	ev.respondWith( // вытаскиваем кеш и пользуемся
-		caches.match(ev.request)
-			.then((response: any) => {
-				if (!response) return
-				return response || fetch(ev.request)// тут пишем стратегию
-				/* В данном случае проверяем кеш "response". Если Там ни чего нет, то идем по url-у запроса */
-			})
-	)
+	console.log('[Request to the server]');
+
+	const url = new URL(ev.request.url);
+
+	if (CACHE_PRIORITY_URLS.includes(url.pathname)) {
+		ev.respondWith(cachePriorityThenFeth(ev));
+		return
+	}
+	if (FETCH_PRIORITY_URLS.includes(ev.request.url)) {
+		ev.respondWith(fetchPriorityThenCache(ev));
+		return
+	}
 });
 
 self.addEventListener('message', (ev: any) => {
