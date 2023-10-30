da637eda3d19f56aea3c813f105a545a05e4df4a
Tryd0g0lik <work80@mail.ru>
2023-10-30 09:01:16 +0700

    [src\app\frontend\src\ts\functions; news-cache; news-public ]: added new modules for old functions

---
+++ src/app/frontend/src/ts/functions/news-public/index.ts
@@ -0,0 +1,48 @@
+// src\app\frontend\src\ts\functions\index.ts
+
+const newCache = require('../news-cache');
+
+
+/**
+ *  Here is function for 2 actions:
+ * 	1 - news sent for cache to the function "newCache": "newCache.cachingData(...)"
+ *  2 - news's posts publications in view an html-box.
+ *
+ * @param news This is data formvat a strimg "{ here-is-your-one-news}"
+ * @returns
+ */
+function publicNews(news: string) {
+	const newsBox = document.querySelector('.news-feed');
+	const newsJson = JSON.parse(news);
+
+	if (!newsJson || typeof newsJson !== 'object') { return }
+	console.log('NEWS before insert: ', newsJson);
+
+	/* ----------cahe---------- */
+	newCache.cachingData(newsJson);
+	/* -------------------- */
+
+	(newsBox as HTMLElement).innerHTML = '';
+	for (let i = 0; i < newsJson['gaz'].length; i++) {
+		const oneNews = newsJson['gaz'];
+		const tenpleteNews = `<div class="news">
+				<div class="data-news">
+					<div class="news-datetime">${oneNews[i]['time']}</div>
+					<div class="news-date">${oneNews[i]['date']}</div>
+				</div>
+				<div class="new-content">
+					<div class="news-content-preview">
+						<img src="./pic/bg_buggy.png" alt="" class="picturies">
+					</div>
+					<div>
+						<p>${oneNews[i]['title']}</p>
+					</div>
+				</div>
+			</div>` as any;
+
+		(newsBox as HTMLElement).insertAdjacentHTML('beforeend', (tenpleteNews as any))
+	}
+}
+
+
+module.exports = { publicNews }
